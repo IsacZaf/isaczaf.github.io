e5aaef4b2529c81961773b9fc83c91e7a421add8
IsacZaf <89772342+IsacZaf@users.noreply.github.com>
2023-02-13 14:03:13 +0100

    Update CardsList.vue

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,51 +1,43 @@
 <template>
-  <div>
-    <h1>Yu-Gi-Oh! Cards List</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-else-if="error">Error: {{ error }}</div>
-    <div v-else>
-      <ul>
-        <li v-for="card in cards" :key="card.id">
-          <strong>{{ card.name }}</strong>
-          <p>{{ card.type }}</p>
-          <p>{{ card.desc }}</p>
-        </li>
-      </ul>
-    </div>
+  <div class="cards-list">
+    <CardsLook v-for="card in cards" :key="card.id" :name="card.name" :type="card.type" :desc="card.desc"
+      :image="card.card_images[0].image_url" />
   </div>
 </template>
 
 <script>
+import CardsLook from "./CardsLook.vue";
+
 export default {
-  name: "CardsList",
+  components: {
+    CardsLook,
+  },
   data() {
     return {
       cards: [],
-      loading: true,
-      error: null,
     };
   },
-  created() {
-    this.fetchCards();
+  async created() {
+    await this.fetchCards();
   },
   methods: {
     async fetchCards() {
       try {
         const response = await fetch(
-          "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+          "https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Red-Eyes"
         );
-        if (!response.ok) {
-          throw new Error(`${response.status} ${response.statusText}`);
-        }
         const data = await response.json();
         this.cards = data.data;
-        this.loading = false;
       } catch (error) {
-        console.error(error);
-        this.error = error.message;
-        this.loading = false;
+        console.log(error);
       }
     },
   },
 };
 </script>
+<style scoped>
+.cards-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
